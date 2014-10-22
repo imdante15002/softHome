dbed1593e9ce92c90017f9f833a470cecc9bc559
imdante15002 <imdante15002@gmail.com>
2014-10-22 17:43:04 +0800

    21041022使用手册完善

--- common/js/manual.js
+++ common/js/manual.js
@@ -2,4 +2,57 @@
  * 使用手册js效果代码
  * @return {[type]} [description]
  */
-$(document).ready(function(){$(".manual-menu ul:first").show();$(".manual-menu ul ol:first").show();$(".manual-menu h3").click(function(){$(this).next("ul").slideToggle(200);$(this).addClass("on").siblings(this).removeClass("on")});$(".manual-menu>ul>li.add").click(function(){$(this).next("ol").slideToggle(200);$(this).toggleClass("del")});$(".manual-menu>ul>ol>li.add").click(function(){$(this).next("ol").slideToggle(200);$(this).toggleClass("del")});$(".manual-menu>ul>ol>ol>li.add").click(function(){$(this).next("ol").slideToggle(200);$(this).toggleClass("del")});$(".menu_right").hide();$(".menu_left").click(function(){$("#aside-menu").animate({width:160},100);$(this).hide();$(".menu_right").show();$(".manual-menu").css("left","160px")});$(".menu_right").click(function(){$("#aside-menu").animate({width:0},100);$(this).hide();$(".menu_left").show();$(".manual-menu").css("left","0")})});
+$(document).ready(function(){
+	$(".manual-menu ul:first").show();
+
+	$(".manual-menu ul ol:first").show();
+
+	$(".manual-menu h3").click(function(){
+		$(this).next("ul").slideToggle(200);
+		$(this).addClass("on").siblings(this).removeClass("on");
+	});
+
+	$(".manual-menu>ul>li.add").click(function(){
+		$(this).next("ol").slideToggle(200);
+		$(this).toggleClass("del");
+	});
+
+	$(".manual-menu>ul>ol>li.add").click(function(){
+		$(this).next("ol").slideToggle(200);
+		$(this).toggleClass("del");
+	});
+
+	$(".manual-menu>ul>ol>ol>li.add").click(function(){
+		$(this).next("ol").slideToggle(200);
+		$(this).toggleClass("del");
+	});
+
+	$(".menu_right").hide();
+
+	$(".menu_left").click(function(){
+		$("#aside-menu").animate({width:160},50);
+		$(this).hide();$(".menu_right").show();
+		$(".manual-menu").css("left","160px");
+		$(".manual-article").css("margin-left","160px");
+	});
+
+	$(".menu_right").click(function(){
+		$("#aside-menu").animate({width:0},100);
+		$(this).hide();
+		$(".menu_left").show();
+		$(".manual-menu").css("left","0");
+		$(".manual-article").css("margin-left","0px");
+	});
+
+	$(".manual-menu ul li a").click(function(){
+		var doc=$(this).attr('id');
+		$(".manual-article").html("<div class='loading'></div>");
+		$.ajax({
+			type:"get",
+			url:"manual/doc/"+doc+".html",
+			success:function(response,xhr){
+				$(".manual-article").html(response);
+			}
+		})
+	})
+});
